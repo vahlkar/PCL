493d38e7301fe40954a0294595a8e3673a30b4f0
jconejero <juan.conejero@pixinsight.com>
2019-10-17 11:39:01 +0200

    ColorSpaces: update module version (part of cumulative bugfix: https://pixinsight.com/forum/index.php?topic=14090.0)

--- src/modules/processes/ColorSpaces/ColorSpacesModule.cpp
+++ src/modules/processes/ColorSpaces/ColorSpacesModule.cpp
@@ -52,13 +52,13 @@
 
 #define MODULE_VERSION_MAJOR     1
 #define MODULE_VERSION_MINOR     1
-#define MODULE_VERSION_REVISION  0
+#define MODULE_VERSION_REVISION  1
 #define MODULE_VERSION_BUILD     0
 #define MODULE_VERSION_LANGUAGE  eng
 
 #define MODULE_RELEASE_YEAR      2019
-#define MODULE_RELEASE_MONTH     9
-#define MODULE_RELEASE_DAY       29
+#define MODULE_RELEASE_MONTH     10
+#define MODULE_RELEASE_DAY       15
 
 #include "ColorSpacesModule.h"
 #include "GrayscaleRGBProcesses.h"
