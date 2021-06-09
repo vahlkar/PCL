cc7751ba504a3900b2fbd4cdcfe5b3cb0b53e68d
jconejero <juan.conejero@pixinsight.com>
2021-06-09 12:17:50 +0200

    INDIClient module 1.2.2: ready for release

--- src/modules/processes/contrib/kkretzschmar/INDIClient/INDIClientModule.cpp
+++ src/modules/processes/contrib/kkretzschmar/INDIClient/INDIClientModule.cpp
@@ -52,13 +52,13 @@
 
 #define MODULE_VERSION_MAJOR     1
 #define MODULE_VERSION_MINOR     2
-#define MODULE_VERSION_REVISION  1
+#define MODULE_VERSION_REVISION  2
 #define MODULE_VERSION_BUILD     0
 #define MODULE_VERSION_LANGUAGE  eng
 
 #define MODULE_RELEASE_YEAR      2021
-#define MODULE_RELEASE_MONTH     5
-#define MODULE_RELEASE_DAY       31
+#define MODULE_RELEASE_MONTH     6
+#define MODULE_RELEASE_DAY       9
 
 #include "INDICCDFrameInterface.h"
 #include "INDICCDFrameProcess.h"
