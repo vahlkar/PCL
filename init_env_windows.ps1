aa2e459eb22d143fc1fb994d7e3fe38d2cf1e73d
Volodia Laniel <volodia@laniel.eu>
2023-08-17 00:15:21 +0200

    Add windows environment init scripts.

---
+++ init_env_windows.ps1
@@ -0,0 +1,7 @@
+$env:PCLDIR = "$env:USERPROFILE\Documents\PixInsight\PCL"
+$env:PCLBINDIR64 = "$env:PCLDIR\bin"
+$env:PCLBINDIR = "$env:PCLBINDIR64"
+$env:PCLLIBDIR64 = "$env:PCLDIR\lib\windows\x64"
+$env:PCLLIBDIR = "$env:PCLLIBDIR64"
+$env:PCLINCDIR = "$env:PCLDIR\include"
+$env:PCLSRCDIR = "$env:PCLDIR\src"
