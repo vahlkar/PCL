389f2647b6219cb6ff9ff6d1e4ce9db0aa71cbe0
Volodia Laniel <volodia@laniel.eu>
2025-01-07 22:48:25 +0100

    Update windows init script.

--- init_env_windows.ps1
+++ init_env_windows.ps1
@@ -1,7 +1,8 @@
-$env:PCLDIR = "$env:USERPROFILE\Documents\PixInsight\PCL"
+$env:PCLDIR = "$env:USERPROFILE\PCL"
 $env:PCLBINDIR64 = "$env:PCLDIR\bin"
 $env:PCLBINDIR = "$env:PCLBINDIR64"
 $env:PCLLIBDIR64 = "$env:PCLDIR\lib\windows\x64"
 $env:PCLLIBDIR = "$env:PCLLIBDIR64"
 $env:PCLINCDIR = "$env:PCLDIR\include"
 $env:PCLSRCDIR = "$env:PCLDIR\src"
+ & "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" $env:PCLSRCDIR\modules\processes\contrib\nvolkov\Blink\windows\vc17\Blink.vcxproj
